90e90b5af4605db5ecd37d0c62bd5436046dbaea
kylepadilla <kylepadilla1@gmail.com>
2019-03-25 16:39:50 -0400

    finished minor formatting and completed the spotify requests

--- liri.js
+++ liri.js
@@ -3,6 +3,9 @@ var axios = require("axios");
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+var moment = require("moment");
+
+var fs = require('fs')
 
 
 var userInput = process.argv.slice(3).join('+');
@@ -19,55 +22,54 @@ break;
 case "movie-this": movie()
 break;
 
-case "do-what-it-says":random()
+case "do-what-it-says": random()
 break;
 }
 
-
 // Node-Spotify-API request
 function music() {
-    
 
     spotify.search({
         type: 'track',
         query: userInput
     }).then(function(data) {
         
-        var spotifyResponse = data
-
-        console.log(spotifyResponse.tracks);
+        var spotifyResponse = data.tracks.items[0]
+        console.log("\n\nSearch Results....");
+        console.log("---------------------------------\n");
+        console.log("Artist: " + spotifyResponse.artists[0].name);
+        console.log("Song name: " + spotifyResponse.name);
+        console.log("Link to song: " + JSON.stringify(spotifyResponse.artists[0].external_urls).slice(11,-1));
+        console.log("Album: " + spotifyResponse.album.name);
+        console.log("\n---------------------------------\n");
       })
       .catch(function(err) {
-        console.log(err);
+        console.log("Spotify Error:" + err);
       })
     }
 
 
-// bands-in-town api request
+// Bands-in-town api request
 function concert() {
 
     var artist = userInput;
-
 
 axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
 .then (
     function(concertResponse){
         
-        // for(i = 0; i < concertResponse.data.length; i++) {
+        for(i = 0; i < concertResponse.data.length; i++) {
 
-        var concert = concertResponse.data
-
-        // console.log(concert)
-
-        console.log(concert)
-        // console.log(concert.venue.region + ", " + concert.venue[i].city)
-        // console.log(concert.venue.datetime)
-    // }
-}
-)
-
-
-}
+            var events = concertResponse.data[i]
+            var eventTime = events.datetime
+            var momentTime = moment(eventTime).format("L"); 
+        console.log("---------------------------------");
+        console.log("Venue: " + events.venue.name)
+        console.log("Location: " + events.venue.city + ", " + events.venue.region + ", " + events.venue.country)
+        console.log("Date: " + momentTime)
+        console.log("---------------------------------");
+    }
+})}
 
 
 // OMDB Request
@@ -79,17 +81,12 @@ var movieInput = "";
         
         movieInput = "mr.nobody"
     }
-    else{
-        movieInput = userInput
-    }
-
+    else{movieInput = userInput}
 axios.get("http://www.omdbapi.com/?t=" + movieInput + "&apikey=7ab220f2&")
-
 .then(
   function(response) {
-
         let movie = response.data
-
+    console.log("\r\n\r\n\r\n");
     console.log("Title: " + JSON.stringify(movie.Title));
     console.log("Realse Year: " + JSON.stringify(movie.Year));
     console.log("IMDB Rating: " + JSON.stringify(movie.imdbRating));
@@ -98,7 +95,18 @@ axios.get("http://www.omdbapi.com/?t=" + movieInput + "&apikey=7ab220f2&")
     console.log("Language: " + JSON.stringify(movie.Languge));
     console.log("Plot: " + JSON.stringify(movie.Plot));
     console.log("Actors: " + JSON.stringify(movie.Actors));
-
-        
+    console.log("\r\n\r\n\r\n");
       })
-    }
+    };
+
+
+// Do-what-it-says function below
+function random() {
+
+    fs.readFile('random.txt',"UTF-8", function(err,data) {
+        if (err) throw err;
+        console.log(data);
+
+    })
+
+}
